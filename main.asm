a2a76cd3dee0f3efa993342976dd2b7f3537f559
TanushKrashak <tankrashak38816@gmail.com>
2025-02-11 22:58:55 +0530

    Can take user input and print it now

--- main.asm
+++ main.asm
@@ -2,22 +2,60 @@
 .stack 100h
 
 data SEGMENT
-    msg DB 'Hello, World!$' ; String to print (terminated with `$` for DOS)                 
+    P1 DB 'Player 1:', '$'
+    P1ClassSelection DB 'Choose Your Class! (Press 1, 2, or 3): ', '$'  
+    YouSelected DB 'You Selected Class ', '$'
 data ENDS
 
-code SEGMENT     
-main:        
+code SEGMENT
+main:
     MOV AX, data
-    MOV DS, AX       
+    MOV DS, AX         ; Initialize Data Segment
+               
+    ; Print P1 MSG           
+    MOV DX, OFFSET P1  
+    MOV AH, 09h        ; DOS print string function
+    INT 21h            ; Print Player 1 message
+    
+    ; Insert line break after MSG
+    MOV DL, 0Dh        ; Carriage Return (CR)
+    MOV AH, 02h        ; DOS function to print a character
+    INT 21h            ; Print Carriage Return
+    MOV DL, 0Ah        ; Line Feed (LF)
+    MOV AH, 02h        ; DOS function to print a character
+    INT 21h            ; Print Line Feed
+          
+    ; Print P1ClassSelection MSG
+    MOV DX, OFFSET P1ClassSelection  
+    MOV AH, 09h        ; DOS print string function
+    INT 21h            ; Print class selection prompt
+    
+    ; Get User Input (Single Character)
+    MOV AH, 01h        ; DOS function to read a character 
+    INT 21h            ; Input from user
+    MOV BX, AX         ; Store input in BX
+        
+    ; Insert a line break after printing input
+    MOV DL, 0Dh        ; Carriage Return (CR)
+    MOV AH, 02h        ; DOS function to print a character
+    INT 21h            ; Print Carriage Return
+    MOV DL, 0Ah        ; Line Feed (LF)
+    MOV AH, 02h        ; DOS function to print a character
+    INT 21h            ; Print Line Feed
+    
+    ; Print the input          
+    MOV DX, OFFSET YouSelected
+    MOV AH, 09h        ; DOS print string function
+    INT 21h            ; Print "You Selected Class"
+    
+    ; Print the input character from BX
+    MOV DL, BL         ; Move the input character to DL for printing
+    MOV AH, 02h        ; DOS function to print a character
+    INT 21h            ; Print the input character
 
-    ; Print "Hello, World!"
-    MOV DX, OFFSET msg  ; Load the address of the string
-    MOV AH, 09h    		; DOS print string function
-    INT 21h             ; Call DOS interrupt
+    ; Exit Program
+    MOV AH, 4Ch        ; DOS function to terminate program
+    INT 21h            ; Exit program
 
-
-    ; Exit program
-    MOV AH, 4Ch        
-    INT 21h         
 code ENDS
-END main     
+END main
