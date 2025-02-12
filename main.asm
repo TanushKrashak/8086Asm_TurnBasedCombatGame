d23eca854bdfea80ea820b4e67b367480894285d
TanushKrashak <tankrashak38816@gmail.com>
2025-02-12 19:38:48 +0530

    Improved Naming of Goto Sections and formatted code

--- main.asm
+++ main.asm
@@ -1,7 +1,10 @@
 .model small
 .stack 100h
 
-data SEGMENT       
+data SEGMENT     
+;==================================================================================
+; PROPERTIES & VARIABLES
+;==================================================================================    
 	; Player 1 Stats
 	P1Health DB 100      
 	P1LightAttackDamage DB 25
@@ -22,29 +25,37 @@ data SEGMENT
     Assassin DB 'Assassin', '$'
     Duelist DB 'Duelist', '$'    
     P1ClassSelection DB 'Choose Your Class! (Press 1-Knight, 2-Assassin, or 3-Duelist): ', '$'  
-    YouSelected DB 'You Selected Class ', '$'
+    YouCheckIf DB 'You Selected Class ', '$'
 data ENDS
-
-code SEGMENT     
-; Function For Printing A Line
-PrintLine:
-	MOV AH, 09h        ; DOS print string function
-    INT 21h            ; Print Msg
- 	RET        
- 
-; Function For Printing A Character
-PrintChar:
-	MOV AH, 02h        ; DOS function to print a character
-    INT 21h            ; Print Carriage Return     
-    RET                                      
- 	
-; Function To take Input From User, Stored in AL
-TakeCharInput:
-	MOV AH, 01h        ; DOS function to read a character 
-    INT 21h            ; Input from user
-    RET         
+      
+      
+code SEGMENT
+;==================================================================================
+; FUNCTIONS
+;==================================================================================    
+	; Function For Printing A Line
+	PrintLine:
+		MOV AH, 09h        ; DOS print string function
+	    INT 21h            ; Print Msg
+	 	RET        
+	 
+	; Function For Printing A Character
+	PrintChar:
+		MOV AH, 02h        ; DOS function to print a character
+	    INT 21h            ; Print Carriage Return     
+	    RET                                      
+	 	
+	; Function To take Input From User, Stored in AL
+	TakeCharInput:
+		MOV AH, 01h        ; DOS function to read a character 
+	    INT 21h            ; Input from user
+	    RET         
+ 	 
  	      	      
 main:
+;==================================================================================
+; MAIN FUNCTION
+;================================================================================== 
     MOV AX, data
     MOV DS, AX         ; Initialize Data Segment
                
@@ -72,24 +83,25 @@ main:
     MOV DL, 0Ah        ; Line Feed (LF)
 	CALL PrintChar
     
-  	; Print Selected Class Name
-    MOV DX, OFFSET YouSelected     
+  	; Print CheckIf Class Name
+    MOV DX, OFFSET YouCheckIf     
     CALL PrintLine       
-    ; Knight 
-    CMP BL,'1'      
-    JNE SelectedAssassin        
-	MOV DX, OFFSET Knight
-	CALL PrintLine 
-	CALL EndClassSelection    
+    ; Knight  
+    CheckIfKnight:
+	    CMP BL,'1'      
+	    JNE CheckIfAssassin        
+		MOV DX, OFFSET Knight
+		CALL PrintLine 
+		CALL EndClassSelection    
 	; Assassin  
-	SelectedAssassin:  	
+	CheckIfAssassin:  	
 	    CMP BL,'2'       
-	    JNE SelectedDuelist:    
+	    JNE CheckIfDuelist:    
     	MOV DX, OFFSET Assassin
     	CALL PrintLine 
     	CALL EndClassSelection 	
 	; Duelist
-	SelectedDuelist:    	
+	CheckIfDuelist:    	
 		CMP BL,'3'       
 		JNE EndClassSelection:		 
     	MOV DX, OFFSET Duelist
