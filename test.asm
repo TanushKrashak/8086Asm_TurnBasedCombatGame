16b615266902ab78a9ee7d424976431e7ef1b3e4
Parth Acharya <1008parth@gmail.com>
2025-02-13 20:59:47 +0530

    Updates bGetChance function

--- test.asm
+++ test.asm
@@ -6,15 +6,15 @@ data SEGMENT
     death_msg db 'You are dead :(', '$'  
     placeholder db 'Alive! ', '$'      
     TotalStats DB 6
-    P1Stats DB 50, 100, 25, 50, 30, 50, 5
+    P1Stats DB 50, 100, 25, 50, 30, 50, 99
     P2Stats DB 50, 100, 25, 50, 30, 50, 20
     Crit_Message DB 'Critical Hit!', '$'
     Hit_Message DB 'Normal Hit!', '$' 
     AllDeadMsg DB 'All players are dead!', '$'
     PlayerCount DB 4  
     CurrentTurn DB 0
-    AliveState DB 11110000B
-    CurrentTurnStats DB 00000000B
+    AliveState DB 00000000B
+    CurrentTurnStats DB 00000000B ; p4_crit, p3_crit, p2_crit, p1_crit
 data ENDS
 
 code SEGMENT     
@@ -46,18 +46,63 @@ code SEGMENT
         MOV BL, [TotalStats]    ; Load length of array for based offset later   
         MOV BH, 0   ;Ensure BX is same as BL in terms of actual value
         MOV AL, [P1Stats + BX]  ;Load crit chance stat into AL
-        CMP AL, DL      ; DL has hundredth of a second
+        CMP AL, DL      ; DL has hundredth of a second 
+        
+        ; Determine current player
+        MOV AH, [CurrentTurn] 
+        MOV AL, [CurrentTurnStats]  ; Load current turn'stats for updation
         JNC GoodLuck ;If current 1/100 of second is less than crit chance, we have critical hit >:)
                
         ; Logic for normal hit
-        MOV DX, offset Hit_Message  
+        MOV DX, offset Hit_Message 
         CALL PrintLine
-        RET
+        CMP AH, 0   ; P1?
+        JE P1_ResetCritical
+        P1_ResetCritical:
+            AND AL, 11101111b ;Reset P1_Crit
+            JMP bGetChance_Final
+        
+        CMP AH, 1
+        JE P2_ResetCritical
+        P2_ResetCritical:
+            AND AL, 11011111b;Reset P2_Crit
+            JMP bGetChance_Final
+        
+        CMP AH, 2
+        JE P3_ResetCritical
+        P3_ResetCritical:
+            AND AL, 10111111b ;Reset P3_Crit
+            JMP bGetChance_Final
+            
+        AND AH, 01111111b    ;  Reset P4_Crit
+        JMP bGetChance_Final    
         
         GoodLuck:
             MOV DX, offset Crit_Message
             CALL PrintLine
-            RET  
+            CMP AH, 0
+            JE P1_SetCritical  
+            P1_SetCritical:
+                OR AL, 00010000b ;Set P1_Crit 
+                JMP bGetChance_Final
+                
+            CMP AH, 1     
+            JE P2_SetCritical
+            P2_SetCritical:
+                OR AL, 00100000b ;Set P2_Crit
+                JMP bGetChance_Final 
+            
+            CMP AH, 2  
+            JE P3_SetCritical
+            P3_SetCritical:
+                OR AL, 01000000b ;Set P3_Crit
+                JMP bGetChance_Final
+            
+            OR AL, 10000000b ;Set P4_Crit
+            
+        bGetChance_Final:
+        MOV [CurrentTurnStats], AL
+        RET  
             
      WrappedIncrement:
         INC AL
@@ -122,12 +167,7 @@ code SEGMENT
          
 main:
     MOV AX, data
-    MOV ds, AX
-    MOV CX, 4  
-    CALL AlternateTurn
-                       
-                       
-end:                    
+    MOV ds, AX                   
     CALL bGetChance
     MOV AH, 4Ch
     INT 21h
