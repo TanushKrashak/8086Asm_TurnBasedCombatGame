aefaac85779e2ca93e0b6eb02e2584af4a2aaeba
Parth Acharya <1008parth@gmail.com>
2025-02-13 22:05:22 +0530

    Merged test functions into main file

--- main.asm
+++ main.asm
@@ -5,9 +5,18 @@ data SEGMENT
 ;==================================================================================
 ; PROPERTIES & VARIABLES
 ;==================================================================================    	
+	; Game Stats
+	TotalStats DB 6
+	
 	; Player Stats
 	Player1Stats  DB 0,0,0,0,0,10  	
 	Player2Stats  DB 0,0,0,0,0,0      
+
+    PlayerCount DB 4	; Number of players, 2 or 4
+    CurrentTurn DB 0	; Indicate which player's turn it is, takes values between 0-3 inclusive
+    AliveState DB 00000000B	; p4_alive, p3_alive, p2_alive, p1_alive
+    CurrentTurnStats DB 00000000B ; p4_crit, p3_crit, p2_crit, p1_crit
+
 
 	; Class Stats (HP, MaxHP, LDmg, HDmg, Def, CC)
 	KnightStats    DB 100, 100,  20,  50,  40,  10  ; Balanced, high defense
@@ -39,6 +48,12 @@ data SEGMENT
     HeavyAttackDamageText DB 0DH, 0AH, 'Heavy Attack Damage: ', '$'
     DefenseText DB 0DH, 0AH, 'Defense: ', '$'
     CriticalChanceText DB 0DH, 0AH, 'Critical Chance: ', '$'
+
+	; In-Combat Texts
+	Crit_Message DB 'Critical Hit!', '$'
+    Hit_Message DB 'Normal Hit!', '$' 
+    AllDeadMsg DB 'All players are dead!', '$'
+
 data ENDS
       
       
@@ -70,8 +85,122 @@ code SEGMENT
 	TakeCharInput:
 		MOV AH, 01h        ; DOS function to read a character 
 	    INT 21h            ; Input from user
-	    RET         
- 	
+	    RET
+
+	; Load system time into CX and DX (CH: Hour, CL: Minute, DH: Second, DL: 1/100th of a second)
+	GetTime:
+        MOV AH, 2CH
+        INT 21H  
+        RET
+                 
+	; Generic random function
+    GetChance:  
+        CALL GetTime   ; Load Counter and Data registers with time data
+        MOV BL, [TotalStats]    ; Load length of array for based offset later   
+        MOV BH, 0   ;Ensure BX is same as BL in terms of actual value
+        MOV AL, [Player1Stats + BX]  ;Load crit chance stat into AL
+        CMP AL, DL      ; DL has hundredth of a second 
+        ; Determine current player
+        MOV AH, CurrentTurn 
+        MOV AL, CurrentTurnStats  ; Load current turn'stats for updation
+        JNC GoodLuck ;If current 1/100 of second is less than crit chance, we have critical hit >:)    
+        ; Logic for normal hit
+        MOV DX, OFFSET Hit_Message 
+        CALL PrintLine
+        CMP AH, 0   ; P1?
+        JNE P1_ResetCritical
+        AND AL, 11101111b ;Reset P1_Crit
+        JMP bGetChance_Final
+        P1_ResetCritical:
+            CMP AH, 1
+            JNE P2_ResetCritical
+            AND AL, 11011111b;Reset P2_Crit
+            JMP bGetChance_Final
+        P2_ResetCritical:
+            CMP AH, 2
+            JNE P3_ResetCritical
+            AND AL, 10111111b ;Reset P3_Crit
+            JMP bGetChance_Final
+        P3_ResetCritical:
+            AND AH, 01111111b    ;  Reset P4_Crit
+            JMP bGetChance_Final    
+        ;Critical Hit
+        GoodLuck:
+            MOV DX, OFFSET Crit_Message
+            CALL PrintLine
+            MOV AH, CurrentTurn 
+            MOV AL, CurrentTurnStats
+            CMP AH, 0
+            JNE P1_SetCritical
+            OR AL, 00010000b ;Set P1_Crit 
+            JMP bGetChance_Final
+            P1_SetCritical:
+                CMP AH, 1     
+                JNE P2_SetCritical
+                OR AL, 00100000b ;Set P2_Crit
+                JMP bGetChance_Final 
+            P2_SetCritical:
+                CMP AH, 2  
+                JNE P3_SetCritical
+                OR AL, 01000000b ;Set P3_Crit
+                JMP bGetChance_Final
+            P3_SetCritical:
+                OR AL, 10000000b ;Set P4_Crit
+        bGetChance_Final:
+        MOV CurrentTurnStats, AL
+        RET 
+
+	; Circular increments current turn
+     WrappedIncrement:
+        INC AL
+        MOV AH, 0
+        MOV BL, PlayerCount
+        DIV BL  ; Remainder in AH is the actual turn number   
+        MOV CurrentTurn, AH   ; Remainder = CurrentTurn 
+        RET
+	
+	; Progress player turns
+	 AlternateTurn:
+        ; Each recursive call increments DH by 1. At DH = 4 (0->1->2->3-->4), we know that 4 recursions were already made. In this case, all players are dead
+        INC DH
+        CMP DH, 4
+        JE AllDead  ; Jump to AllDead to prevent infinite recursion
+        MOV AL, CurrentTurn
+        CALL WrappedIncrement   ; Increment AL, wrap if necessary
+        MOV DL, AliveState    ; Load AliveState byte into DL to check if the current player is dead or not
+        ; AH holds current turn
+        CMP AH, 0
+        JE P1_Turn
+        CMP AH, 1
+        JE P2_Turn        
+        CMP AH, 2
+        JE P3_Turn   
+        ; JZ ensures that if the CurrentTurn's player is dead (nth bit = 0), we alternate turn again                
+        P4_Turn:
+        TEST DL, 10000000b  ;Check if P4 is alive
+        JZ AlternateTurn                            
+        JMP Final
+        P3_Turn:
+        TEST DL, 01000000b  ;Check if P3 is alive    
+        JZ AlternateTurn
+        JMP Final
+        P2_Turn:
+        TEST DL, 00100000b  ;Check if P2 is alive
+        JZ AlternateTurn
+        JMP Final
+        P1_Turn:
+        TEST DL, 00010000b  ;Check if P1 is alive
+        JZ AlternateTurn
+        JMP Final
+        Final:
+        MOV DH, 0
+        RET
+        ; Exception Case: All players are dead
+        AllDead:
+        MOV DX, OFFSET AllDeadMsg
+        CALL PrintLine
+        CALL PrintNewLine
+        RET
  	; This function converts an Integer to a String and then prints it
  	; Each digit in the int has to be scanned individually and then 
  	; you have to add '0' to convert it to a Character 
@@ -217,7 +346,6 @@ main:
    	CALL LoadPlayerStats    
 	; Print Player Stats  
 	MOV SI, OFFSET Player2Stats
-    LEA SI, Player2Stats
     CALL PrintPlayerStats  
     
                    
